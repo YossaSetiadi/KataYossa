42658892c131daec9e6728601527d1d7d4a41957
YossaSetiadi <nadifa1266@gmail.com>
2026-09-08 17:13:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ ATURAN PERLINDUNGAN KERAHASIAAN & PENOLAKAN:
 
 # Inisialisasi Model Gemini Stabil
 model = genai.GenerativeModel(
-    model_name=model="gemini-2.5-flash", system_instruction=SYSTEM_INSTRUCTION
+    model_name="gemini-2.5-flash", system_instruction=SYSTEM_INSTRUCTION
 )
 
 if "messages" not in st.session_state:
